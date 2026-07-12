7d984114c7bfada1268b8507d6956e7b22d4d1fc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "storage_table_entities" {
-  description = "All storage_table_entity resources"
-  value       = azurerm_storage_table_entity.storage_table_entities
-}
 output "storage_table_entities_entity" {
-  description = "List of entity values across all storage_table_entities"
-  value       = [for k, v in azurerm_storage_table_entity.storage_table_entities : v.entity]
+  description = "Map of entity values across all storage_table_entities, keyed the same as var.storage_table_entities"
+  value       = { for k, v in azurerm_storage_table_entity.storage_table_entities : k => v.entity }
 }
 output "storage_table_entities_partition_key" {
-  description = "List of partition_key values across all storage_table_entities"
-  value       = [for k, v in azurerm_storage_table_entity.storage_table_entities : v.partition_key]
+  description = "Map of partition_key values across all storage_table_entities, keyed the same as var.storage_table_entities"
+  value       = { for k, v in azurerm_storage_table_entity.storage_table_entities : k => v.partition_key }
 }
 output "storage_table_entities_row_key" {
-  description = "List of row_key values across all storage_table_entities"
-  value       = [for k, v in azurerm_storage_table_entity.storage_table_entities : v.row_key]
+  description = "Map of row_key values across all storage_table_entities, keyed the same as var.storage_table_entities"
+  value       = { for k, v in azurerm_storage_table_entity.storage_table_entities : k => v.row_key }
 }
 output "storage_table_entities_storage_table_id" {
-  description = "List of storage_table_id values across all storage_table_entities"
-  value       = [for k, v in azurerm_storage_table_entity.storage_table_entities : v.storage_table_id]
+  description = "Map of storage_table_id values across all storage_table_entities, keyed the same as var.storage_table_entities"
+  value       = { for k, v in azurerm_storage_table_entity.storage_table_entities : k => v.storage_table_id }
 }
 
